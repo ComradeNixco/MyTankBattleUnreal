f27e66dc14ca54b3fa1836268e0b2a7ba2e20d0e
Nicolas Lapointe <nixcocpp@gmail.com>
2017-06-14 22:26:18 -0400

    ReAdded UProject

---
+++ MyTankBattleUnreal/Source/MyTankBattleUnreal/MyTankBattleUnrealGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyTankBattleUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTANKBATTLEUNREAL_API AMyTankBattleUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
